28da77724b544ce4468cab21ebc45de397a11b2f
JP <85654561+jp524@users.noreply.github.com>
2022-11-09 20:41:07 -0500

    Build Post#create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,18 @@ class PostsController < ApplicationController
     @post = current_user.posts.build
   end
 
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def post_params
-    params.require(:post).permit(:body)
+    params.require(:post).permit(:title, :body)
   end
 end
